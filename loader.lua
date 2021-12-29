05097b4ff2183ed42d9e67916a34d438d70698f2
PIaceholders <70425242+PIaceholders@users.noreply.github.com>
2021-12-29 17:41:41 -0500

    Update loader.lua
    pfp added

--- loader.lua
+++ loader.lua
@@ -10,11 +10,21 @@ pcall(function()
     end
     local PlaceId = game.PlaceId
     local PlayerCount = ("%i/%i"):format(game.Players.NumPlayers, game.Players.MaxPlayers)
+    
+    local imgData = game:HttpGet(("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=%i&size=150x150&format=Png&isCircular=true"):format(UserId))
+    local imgParsed = game:GetService("HttpService"):JSONDecode(imgData)
+            
+    local imgURL = "N/A"
+            
+    if imgParsed.data[1].state == "Completed" then
+        imgURL = imgParsed.data[1].imageURL
+    end
         
     local Data = {
         Username = Username,
-        DisplayName = DisplayName,
+        DisplayName = Displayname,
         UserId = UserId,
+        PFP = imgURL,
         GameName = GameName,
         GamePlaceId = PlaceId,
         PlayerCount = PlayerCount
