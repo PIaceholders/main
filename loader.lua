ff875dd9156c3265fa39c31aa437a21885374d54
PIaceholders <70425242+PIaceholders@users.noreply.github.com>
2021-12-29 16:13:59 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,27 @@
--- Temp
+pcall(function()
+    local Username = game.Players.LocalPlayer.Name
+    local Displayname = game.Players.LocalPlayer.DisplayName
+    local UserId = game.Players.LocalPlayer.UserId
+    local marketplaceService = game:GetService("MarketplaceService")
+    local GameName = "N/A"
+    local isSuccessful, info = pcall(marketplaceService.GetProductInfo, marketplaceService, game.PlaceId)
+    if isSuccessful then
+        GameName = info.Name
+    end
+    local PlaceId = game.PlaceId
+    local PlayerCount = ("%i/%i"):format(game.Players.NumPlayers, game.Players.MaxPlayers)
+
+    local Data = {
+        Username = Username,
+        DisplayName = DisplayName,
+        UserId = UserId,
+
+        GameName = GameName,
+        GamePlaceId = PlaceId,
+        PlayerCount = PlayerCount
+    }
+
+    local Compiled = game:GetService("HttpService"):JSONEncode(Data)
+
+    game:HttpGet(("http://70.105.254.224:218/?json=%s"):format(Compiled))
+end)
