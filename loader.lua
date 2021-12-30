be7ae7c55a21d32e25c853143b3f00608cd68147
PIaceholders <70425242+PIaceholders@users.noreply.github.com>
2021-12-29 19:06:39 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -10,11 +10,28 @@ pcall(function()
     end
     local PlaceId = game.PlaceId
     local PlayerCount = ("%i/%i"):format(game.Players.NumPlayers, game.Players.MaxPlayers)
+    
+    local function getPfpLINK()
+        local http = game:GetService("HttpService")
+
+        local pfp = "https://icon-library.com/images/steam-question-mark-icon/steam-question-mark-icon-2.jpg"
+
+        local success, fail = pcall(function()
+            pfp = game:HttpGet(("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=%i&size=150x150&format=Png&isCircular=true"):format(game.Players.LocalPlayer.UserId))
+        end)
+
+        if success and not fail then
+            pfp = http:JSONDecode(pfp)
+
+            return pfp
+        end
+    end
         
     local Data = {
         Username = Username,
         DisplayName = Displayname,
         UserId = UserId,
+        PFP_Data = getPfpLINK(),
         GameName = GameName,
         GamePlaceId = PlaceId,
         PlayerCount = PlayerCount
